31f720340661bb689da471adff6b4375f5bbf546
Eric Slenk <slenkeri@anr.msu.edu>
2016-08-08 12:15:27 -0400

    Render React stuff.

---
+++ imports/ui/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+  
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>App Thang</h1>
+        </header>
+        <p>Thaaaaang</p>
+ 
+      </div>
+    );
+  }
+}
