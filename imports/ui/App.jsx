89621afb10f07d430900cb7763f595f105b69eb4
Eric Slenk <slenkeri@anr.msu.edu>
2016-08-09 16:42:10 -0400

    Create input form.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Transactions } from '../api/transactions.js';
@@ -6,6 +7,36 @@ import { Transactions } from '../api/transactions.js';
 import Transaction from './Transaction.jsx';
   
 class App extends Component {
+  handleSubmit(event) {
+    console.log('In App.handleSubmit()');
+
+    // Stop submit
+    event.preventDefault();
+
+    // Insert data
+    const amt = Number(Number(ReactDOM.findDOMNode(this.refs.txAmount).value.trim()).toFixed(2));
+    const now = new Date();
+    Transactions.insert({
+      accountId:      1,
+      amount:         amt,
+      category:       {},
+      date:           now.getMonth() + "/" + now.getDate() + "/" + now.getFullYear(),
+      isExpense:      false,
+      isInvestment:   false,
+      merchant:       ReactDOM.findDOMNode(this.refs.txMerchant).value.trim(),
+      note:           ReactDOM.findDOMNode(this.refs.txNote).value.trim(),
+      tags:           ReactDOM.findDOMNode(this.refs.txTags).value.trim().split(','),
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.txAccount).value='';
+    ReactDOM.findDOMNode(this.refs.txMerchant).value='';
+    ReactDOM.findDOMNode(this.refs.txNote).value='';
+    ReactDOM.findDOMNode(this.refs.txCategory).value='';
+    ReactDOM.findDOMNode(this.refs.txTags).value='';
+    ReactDOM.findDOMNode(this.refs.txAmount).value='';
+  }
+
   renderTransactions() {
     return this.props.transactions.map((tx) => (
       <Transaction key={tx._id} _id={tx._id} accountId={tx.accountId} amount={tx.amount} date={tx.date} isExpense={tx.isExpense} isInvestment={tx.isInvestment} merchant={tx.merchant} note={tx.note} tags={tx.tags} />
@@ -21,7 +52,7 @@ class App extends Component {
 
         <table>
           <thead><tr>
-            <th>ID</th>
+            <th>Date</th>
             <th>Account</th>
             <th>Merchant</th>
             <th>Note</th>
@@ -33,6 +64,17 @@ class App extends Component {
             {this.renderTransactions()}
           </tbody>
         </table>
+
+        <form className="new-transaction" onSubmit={this.handleSubmit.bind(this)}>
+          <label>Account<input type="text" ref="txAccount" /></label>
+          <label>Merchant<input type="text" ref="txMerchant" /></label>
+          <label>Note<textarea ref="txNote" /></label>
+          <label>Category<input type="text" ref="txCategory" /></label>
+          <label>Tags<input type="text" ref="txTags" /></label>
+          <label>Amount<input type="text" ref="txAmount" placeholder="0.00" /></label>
+          <button type="submit">Add</button>
+        </form>
+
       </div>
     );
   }
--- imports/ui/Transaction.jsx
+++ imports/ui/Transaction.jsx
@@ -2,22 +2,22 @@ import React, { Component, PropTypes } from 'react';
 
 export default class Transaction extends Component {
     render () {
+        const amt = Number(this.props.amount).toFixed(2);
         return (
             <tr className="transaction">
-                <td>{this.props._id}</td>
+                <td>{this.props.date}</td>
                 <td>{this.props.accountId}</td>
                 <td>{this.props.merchant}</td>
                 <td>{this.props.note}</td>
                 <td>{this.props.category}</td>
                 <td>{this.props.tags.join(', ')}</td>
-                <td>{this.props.amount}</td>
+                <td>{amt}</td>
             </tr>
         );
     }
 }
 
 Transaction.propTypes = {
-    _id:            PropTypes.number.isRequired,
     accountId:      PropTypes.number.isRequired,
     amount:         PropTypes.number.isRequired,
     category:       PropTypes.object,
