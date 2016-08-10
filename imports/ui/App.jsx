3a955529eea3073ee23127a66d20cdcb836ca18b
Eric Slenk <slenkeri@anr.msu.edu>
2016-08-10 10:56:23 -0400

    Make each Transaction its own edit form.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,52 +7,15 @@ import { Transactions } from '../api/transactions.js';
 import TransactionTable from './TransactionTable.jsx';
   
 class App extends Component {
-  handleSubmit(event) {
-    // Stop submit
-    event.preventDefault();
-
-    // Insert data
-    const amt = Number(Number(ReactDOM.findDOMNode(this.refs.txAmount).value.trim()).toFixed(2));
-    const now = new Date();
-    Transactions.insert({
-      accountId:      1,
-      amount:         amt,
-      category:       {},
-      date:           now.getMonth() + "/" + now.getDate() + "/" + now.getFullYear(),
-      isExpense:      false,
-      isInvestment:   false,
-      merchant:       ReactDOM.findDOMNode(this.refs.txMerchant).value.trim(),
-      note:           ReactDOM.findDOMNode(this.refs.txNote).value.trim(),
-      tags:           ReactDOM.findDOMNode(this.refs.txTags).value.trim().split(','),
-    });
-
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.txAccount).value='';
-    ReactDOM.findDOMNode(this.refs.txMerchant).value='';
-    ReactDOM.findDOMNode(this.refs.txNote).value='';
-    ReactDOM.findDOMNode(this.refs.txCategory).value='';
-    ReactDOM.findDOMNode(this.refs.txTags).value='';
-    ReactDOM.findDOMNode(this.refs.txAmount).value='';
-  }
-
   render() {
     return (
       <div className="container">
+      
         <header>
           <h1>App Thang</h1>
         </header>
 
         <TransactionTable transactions={this.props.transactions} />
-
-        <form className="new-transaction" onSubmit={this.handleSubmit.bind(this)}>
-          <label>Account<input type="text" ref="txAccount" /></label>
-          <label>Merchant<input type="text" ref="txMerchant" /></label>
-          <label>Note<textarea ref="txNote" /></label>
-          <label>Category<input type="text" ref="txCategory" /></label>
-          <label>Tags<input type="text" ref="txTags" /></label>
-          <label>Amount<input type="text" ref="txAmount" placeholder="0.00" /></label>
-          <button type="submit">Add</button>
-        </form>
 
       </div>
     );
--- imports/ui/Transaction.jsx
+++ imports/ui/Transaction.jsx
@@ -1,19 +1,58 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Transaction extends Component {
+    onEditClicked(event) {
+        console.log("Edit clicked");
+    }
+
+    onSaveClicked(event) {
+        console.log("Save clicked");
+    }
+
+    onCancelClicked(event) {
+        console.log("Cancel clicked");
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        // Insert data
+        const amt = Number(Number(ReactDOM.findDOMNode(this.refs.txAmount).value.trim()).toFixed(2));
+        const now = new Date();
+        const newData = {
+          accountId:      1,
+          amount:         amt,
+          category:       {},
+          date:           now.getMonth() + "/" + now.getDate() + "/" + now.getFullYear(),
+          isExpense:      false,
+          isInvestment:   false,
+          merchant:       ReactDOM.findDOMNode(this.refs.txMerchant).value.trim(),
+          note:           ReactDOM.findDOMNode(this.refs.txNote).value.trim(),
+          tags:           ReactDOM.findDOMNode(this.refs.txTags).value.trim().split(','),
+        };
+        Transactions.insert(newData);
+    }
+
     render () {
         const amt = Number(this.props.amount).toFixed(2);
         const formId = "tx-form-" + this.props._id;
         return (
             <tr className="transaction">
-                <td>{this.props.date}</td>
-                <td>{this.props.accountId}</td>
-                <td>{this.props.merchant}</td>
-                <td>{this.props.note}</td>
-                <td>{this.props.category}</td>
-                <td>{this.props.tags.join(', ')}</td>
-                <td>{amt}</td>
-                <td><form id={formId}><input type="hidden" name="id" value={this.props._id} /></form></td>
+                <td><input form={formId} type="text" placeholder="MM/DD/YYYY" defaultValue={this.props.date} /></td>
+                <td><input form={formId} type="text" placeholder="#" defaultValue={this.props.accountId} /></td>
+                <td><input form={formId} type="text" placeholder="Merchant" defaultValue={this.props.merchant} /></td>
+                <td><input form={formId} type="text" placeholder="Note" defaultValue={this.props.note} /></td>
+                <td><input form={formId} type="text" placeholder="Category" defaultValue={this.props.category} /></td>
+                <td><input form={formId} type="text" placeholder="Tags" defaultValue={this.props.tags.join(', ')} /></td>
+                <td><input form={formId} type="text" placeholder="0.00" defaultValue={amt} /></td>
+                <td>
+                    <form id={formId} className="tx-form" onSubmit={this.onSubmit.bind(this)}>
+                        <input type="hidden" ref="txId" name="id" value={this.props._id} />
+                        <button type="button" className="tx-edit" onClick={this.onEditClicked.bind(this)}>Edit</button>
+                        <button type="button" className="tx-save" onClick={this.onSaveClicked.bind(this)}>Save</button>
+                        <button type="button" className="tx-cancel" onClick={this.onCancelClicked.bind(this)}>Cancel</button>
+                    </form>
+                </td>
             </tr>
         );
     }
